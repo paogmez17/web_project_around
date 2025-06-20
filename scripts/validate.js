204629a5cb13b2564873db249db92d1a967aa7fe
Paola <paogomez17@hotmail.com>
2025-06-20 09:46:55 -0500

    arreglos vista movil

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,60 +1,73 @@
+const settigns = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
 // Mostrar mensaje de error
 
-function showInputError(formElement, formInput, errorMessage) {
-  const errorElement = formElement.querySelector(`#${formInput.id}-error`);
-  formInput.classList.add("popup__input_type_error");
+function showInputError(formSelector, inputSelector, errorMessage) {
+  const errorElement = formSelector.querySelector(`#${inputSelector.id}-error`);
+  inputSelector.classList.add("popup__input_type_error");
   errorElement.textContent = errorMessage;
   errorElement.classList.add("popup__input-error_visible");
 }
 
 // Ocultar mensaje de error
 
-function hideInputError(formElement, formInput) {
-  const errorElement = formElement.querySelector(`#${formInput.id}-error`);
-  formInput.classList.remove("popup__input_type_error");
+function hideInputError(formSelector, inputSelector) {
+  const errorElement = formSelector.querySelector(`#${inputSelector.id}-error`);
+  inputSelector.classList.remove("popup__input_type_error");
   errorElement.textContent = "";
   errorElement.classList.remove("popup__input-error_visible");
 }
 
 // Verificar si un input es válido
-function checkInputValidity(formElement, formInput) {
-  if (!formInput.validity.valid) {
-    showInputError(formElement, formInput, formInput.validationMessage);
+function checkInputValidity(formSelector, inputSelector) {
+  if (!inputSelector.validity.valid) {
+    showInputError(
+      formSelector,
+      inputSelector,
+      inputSelector.validationMessage
+    );
   } else {
-    hideInputError(formElement, formInput);
+    hideInputError(formSelector, inputSelector);
   }
 }
 
 // Verificar si hay inputs inválidos
 function hasInvalidInput(inputList) {
-  return inputList.some((formInput) => !formInput.validity.valid);
+  return inputList.some((inputSelector) => !inputSelector.validity.valid);
 }
 
 // Habilitar o deshabilitar el botón
-function toggleButtonState(inputList, buttonElement) {
-  buttonElement.disabled = hasInvalidInput(inputList);
+function toggleButtonState(inputList, submitButtonSelector) {
+  submitButtonSelector.disabled = hasInvalidInput(inputList);
 }
 
 // Asignar listeners a los inputs de un formulario
-function setEventListeners(formElement) {
-  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-  const buttonElement = formElement.querySelector(".popup__button");
+function setEventListeners(formSelector) {
+  const inputList = Array.from(formSelector.querySelectorAll(".popup__input"));
+  const submitButtonSelector = formSelector.querySelector(".popup__button");
 
-  inputList.forEach((formInput) => {
-    formInput.addEventListener("input", function () {
-      checkInputValidity(formElement, formInput);
-      toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputSelector) => {
+    inputSelector.addEventListener("input", function () {
+      checkInputValidity(formSelector, inputSelector);
+      toggleButtonState(inputList, submitButtonSelector);
     });
   });
 
-  toggleButtonState(inputList, buttonElement); // Estado inicial del botón
+  toggleButtonState(inputList, submitButtonSelector); // Estado inicial del botón
 }
 
 // Función principal que activa validación en todos los formularios
 function enableValidation() {
   const formList = Array.from(document.querySelectorAll(".popup__form"));
-  formList.forEach((formElement) => {
-    setEventListeners(formElement);
+  formList.forEach((formSelector) => {
+    setEventListeners(formSelector);
   });
 }
 
